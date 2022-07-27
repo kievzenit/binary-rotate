11a1e5cb4139b4e2d40c679472f6e5ad90644a24
kievzenit <contact@kievzenit.tk>
2022-07-27 17:14:14 +0300

    Version 1.0.0 of BROT (#1)
    * Binary rotation class added
    
    * Unit tests for BROT class
    
    * files moved into BROT dir
    
    * NDESK library deleted
    
    * version 0.1.0
    
    * Fixed not working multiple input handler
    
    * Directory files rotation support added
    
    * Moved BROT class to Utils folder
    
    * Moved business logic to CMD class
    
    * Default rotation degree and defualt rotation direction added
    
    * CMD initialization to entry point added
    
    * version 1.0.0

---
+++ BROT.Tests/BROTClassTest.cs
@@ -0,0 +1,68 @@
+namespace BROT.Tests
+{
+    [TestClass]
+    public class BROTClassTest
+    {
+        const byte ROTATION_DEGREE = 13;
+
+        [TestMethod]
+        public void PositiveRotate_255_With_13degrees_ShouldBe_12()
+        {
+            //arrange
+            var data = new byte[1] { 255 };
+            const byte expected = 12;
+            const bool isPositiveRotation = true;
+
+            //act
+            var actual = BROT.Rotate(data, ROTATION_DEGREE, isPositiveRotation);
+
+            //assert
+            Assert.AreEqual(expected, actual[0]);
+        }
+
+        [TestMethod]
+        public void PositiveRotate_0_With_13degrees_ShouldBe_13()
+        {
+            //arrange
+            var data = new byte[1] { 0 };
+            const byte expected = 13;
+            const bool isPositiveRotation = true;
+
+            //act
+            var actual = BROT.Rotate(data, ROTATION_DEGREE, isPositiveRotation);
+
+            //assert
+            Assert.AreEqual(expected, actual[0]);
+        }
+
+        [TestMethod]
+        public void NegativeRotate_12_With_13degrees_ShouldBe_255()
+        {
+            //arrange
+            var data = new byte[1] { 12 };
+            const byte expected = 255;
+            const bool isPositiveRotation = false;
+
+            //act
+            var actual = BROT.Rotate(data, ROTATION_DEGREE, isPositiveRotation);
+
+            //assert
+            Assert.AreEqual(expected, actual[0]);
+        }
+
+        [TestMethod]
+        public void NegativeRotate_13_With_13degrees_ShouldBe_0()
+        {
+            //arrange
+            var data = new byte[1] { 13 };
+            const byte expected = 0;
+            const bool isPositiveRotation = false;
+
+            //act
+            var actual = BROT.Rotate(data, ROTATION_DEGREE, isPositiveRotation);
+
+            //assert
+            Assert.AreEqual(expected, actual[0]);
+        }
+    }
+}
---
+++ BROT/Program.cs
@@ -0,0 +1,26 @@
+﻿using BROT.Utils;
+
+namespace BROT
+{
+    internal class Program
+    {
+        const string AUTHOR = "kievzenit";
+        const string LICENSE = "MIT";
+        const byte MAJOR_VERSION = 1;
+        const byte MINOR_VERSION = 0;
+        const byte PATCH_VERSION = 0;
+
+        static void Main(string[] args)
+        {
+            var cmd = new CMD(
+                args,
+                MAJOR_VERSION,
+                MINOR_VERSION,
+                PATCH_VERSION,
+                AUTHOR,
+                LICENSE);
+
+            cmd.Run();
+        }
+    }
+}
